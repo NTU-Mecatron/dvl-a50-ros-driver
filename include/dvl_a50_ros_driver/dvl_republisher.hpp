4ce31276b85dda0f2bf78924530465deb812a803
minh <minhnhangia@gmail.com>
2026-01-19 20:48:40 +0800

    refactor dvl republisher

--- include/dvl_a50_ros_driver/dvl_republisher.hpp
+++ include/dvl_a50_ros_driver/dvl_republisher.hpp
@@ -4,6 +4,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "geometry_msgs/msg/twist_with_covariance_stamped.hpp"
+#include <nlohmann/json.hpp>
 
 namespace dvl_a50_ros_driver
 {
@@ -15,11 +16,24 @@ namespace dvl_a50_ros_driver
 
   private:
     void raw_dvl_callback(const std_msgs::msg::String::SharedPtr msg);
+    
+    // Initialization helpers
+    void load_parameters();
+    void setup_pub_sub();
+    void initialize_twist_template();
+    
+    // Message processing helpers
+    bool is_velocity_valid(const nlohmann::json& data);
+    void populate_linear_velocities(geometry_msgs::msg::TwistWithCovarianceStamped& twist_msg,
+                                   const nlohmann::json& data) const;
+    void compute_velocity_covariances(double& variance_x, double& variance_y, double& variance_z,
+                                     const nlohmann::json& data) const;
 
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr dvl_raw_sub_;
     rclcpp::Publisher<geometry_msgs::msg::TwistWithCovarianceStamped>::SharedPtr dvl_twist_pub_;
 
     std::string dvl_frame_id_;
+    geometry_msgs::msg::TwistWithCovarianceStamped twist_template_;
     bool use_original_covariance_;
     bool use_fom_to_compute_covariance_;
     double linear_vel_var_x_;
